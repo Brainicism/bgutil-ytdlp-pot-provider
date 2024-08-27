68b70a805340f411898b5adb9c1fc02c7929d1d5
Brian Le <brainicism@gmail.com>
2024-08-27 19:37:47 +0000

    Support application/x-www-form-urlencoded

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import fastify from "fastify";
 
 const httpServer = fastify({});
 
+httpServer.register(require("@fastify/formbody"));
+
 httpServer.post("/get_pot", {}, async (request, reply) => {
   const sessionManager = new SessionManager();
   const x = await sessionManager.generatePoToken(
