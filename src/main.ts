c98c1ba377a4d7140951edab232bdebdd61892ce
Brian Le <brainicism@gmail.com>
2024-08-27 19:53:45 +0000

    disable formbody

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import fastify from "fastify";
 
 const httpServer = fastify({});
 
-httpServer.register(require("@fastify/formbody"));
+// httpServer.register(require("@fastify/formbody"));
 
 httpServer.post("/get_pot", {}, async (request, reply) => {
   const sessionManager = new SessionManager();
