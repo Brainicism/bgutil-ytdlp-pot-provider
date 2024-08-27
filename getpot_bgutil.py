dea1ec6d2808ebd955a67f5f7d00bfb17c36b9e8
Brian Le <brainicism@gmail.com>
2024-08-27 22:00:52 +0000

    Update port numnber in plugin

--- getpot_bgutil.py
+++ getpot_bgutil.py
@@ -9,7 +9,6 @@ from yt_dlp_plugins.extractor.getpot import GetPOTProvider, register_provider, r
 @register_provider
 class BgUtilPotProviderRH(GetPOTProvider):
     _PROVIDER_NAME = 'BgUtilPot'
-
     _SUPPORTED_CLIENTS = ('web_creator', 'web', 'web_embedded', 'web_music')
 
     def _validate_get_pot(self, client: str, ydl: YoutubeDL, visitor_data=None, data_sync_id=None, player_url=None, **kwargs):
@@ -17,7 +16,7 @@ class BgUtilPotProviderRH(GetPOTProvider):
             raise UnsupportedRequest('Fetching PO Token for accounts is not supported')
 
     def _get_pot(self, client: str, ydl: YoutubeDL, visitor_data=None, data_sync_id=None, player_url=None, **kwargs) -> str:
-        response = ydl.urlopen(Request('http://127.0.0.1:8080/get_pot', data=json.dumps({
+        response = ydl.urlopen(Request('http://127.0.0.1:4416/get_pot', data=json.dumps({
             'client': client,
             'visitor_data': visitor_data
         }).encode(), headers = {'Content-Type': 'application/json'}))
