f3fbb28b037b6cbca131d2a2ff6640f0683f642e
Brian Le <brainicism@gmail.com>
2024-12-25 16:04:49 +0000

    Handle exceptions from generatePoToken

--- server/src/main.ts
+++ server/src/main.ts
@@ -51,14 +51,22 @@ httpServer.post("/get_pot", async (request, response) => {
         visitIdentifier = generatedVisitorData;
     }
 
-    const sessionData = await sessionManager.generatePoToken(
-        visitIdentifier,
-        proxy,
-    );
-    response.send({
-        po_token: sessionData.poToken,
-        visit_identifier: sessionData.visitIdentifier,
-    });
+    try {
+        const sessionData = await sessionManager.generatePoToken(
+            visitIdentifier,
+            proxy,
+        );
+
+        response.send({
+            po_token: sessionData.poToken,
+            visit_identifier: sessionData.visitIdentifier,
+        });
+    } catch (e) {
+        console.error(
+            `Failed while generating POT. err = ${JSON.stringify(e)}`,
+        );
+        response.status(500).send({ error: JSON.stringify(e) });
+    }
 });
 
 httpServer.post("/invalidate_caches", async (request, response) => {
