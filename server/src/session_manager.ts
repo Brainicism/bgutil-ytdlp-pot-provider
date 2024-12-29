ff7118cfc99c237a2790754fafb6a1544cfe9b1a
Brian Le <brainicism@gmail.com>
2024-12-29 00:09:00 -0500

    Use bgutils default user agent in requests

--- server/src/session_manager.ts
+++ server/src/session_manager.ts
@@ -169,11 +169,7 @@ export class SessionManager {
                 for (let attempts = 1; attempts <= maxRetries; attempts++) {
                     try {
                         const response = await axios.post(url, options.body, {
-                            headers: {
-                                ...options.headers,
-                                "User-Agent":
-                                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-                            },
+                            headers: options.headers,
                             httpsAgent: dispatcher,
                         });
 
