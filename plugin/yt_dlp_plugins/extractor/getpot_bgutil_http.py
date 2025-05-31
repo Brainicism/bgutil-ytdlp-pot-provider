50b5865ec18f9519f2cb939525176b8cc9aae12f
Brian Le <brainicism@gmail.com>
2025-05-31 11:24:48 -0700

    print response

--- plugin/yt_dlp_plugins/extractor/getpot_bgutil_http.py
+++ plugin/yt_dlp_plugins/extractor/getpot_bgutil_http.py
@@ -137,7 +137,7 @@ class BgUtilHTTPPTP(BgUtilPTPBase):
             raise PoTokenProviderError(error_msg)
         if 'poToken' not in response_json:
             raise PoTokenProviderError(
-                'Server did not respond with a poToken')
+                f'Server did not respond with a poToken. Received response: {json.dumps(response_json)}')
 
         po_token = response_json['poToken']
         self.logger.trace(f'Generated POT: {po_token}')
