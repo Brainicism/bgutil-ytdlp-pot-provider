ad6f31cb45b58af2bc361abe8a0020982fe426a6
Brian Le <brainicism@gmail.com>
2026-03-12 12:50:39 -0700

    Add GET / route with warning message

--- server/src/main.ts
+++ server/src/main.ts
@@ -54,6 +54,12 @@ httpServer
     });
 
 const sessionManager = new SessionManager();
+httpServer.get("/", async (request, response) => {
+    response.status(400).send({
+        error: "This server is not meant to be accessed directly unless you know what you're doing. Follow the README for plugin/provider setup, and yt-dlp will automatically use the provider: https://github.com/Brainicism/bgutil-ytdlp-pot-provider#readme",
+    });
+});
+
 httpServer.post("/get_pot", async (request, response) => {
     const body = request.body || {};
     if (body.data_sync_id)
